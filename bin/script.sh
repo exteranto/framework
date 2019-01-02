d2f1955caf3fbe8e809866d1bee5c53614abf5f8
Michael Bausano <michael@joinpouch.com>
2019-01-02 16:36:56 +0000

    indent

--- bin/script.sh
+++ bin/script.sh
@@ -7,10 +7,10 @@ do
   cd ${lib}
   # Start commands, use ${lib} to refer to the current package.
 
-  if ! npm i || ! npm run build || ! npm run test
-  then
-    exit 1
-  fi
+    if ! npm i || ! npm run build || ! npm run test
+    then
+      exit 1
+    fi
 
   # End commands.
   cd ..
