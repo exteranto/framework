f207faa3e02d8764f14da4a1d46838d4938c2388
Michael Bausano <michael@joinpouch.com>
2019-01-02 11:40:51 +0000

    Safari installed/updated event

--- lib/management/src/runtime/safari/events.ts
+++ lib/management/src/runtime/safari/events.ts
@@ -1,6 +1,34 @@
 import { Dispatcher } from '@exteranto/events'
+import { Container } from '@exteranto/ioc'
 
 export const register: (dispatcher: Dispatcher) => void = (dispatcher) => {
-  const event = 'installed'
-  dispatcher.fire(`app.management.runtime.${event}`, {})
+  const exteranto: any = getExterantoInfo()
+  const version: string = Container.resolveParam('app.version')
+
+  if (exteranto.version === version) {
+    return
+  }
+
+  const event = exteranto.version ? 'installed' : 'updated'
+
+  dispatcher.fire(`app.management.runtime.${event}`, {
+    previousVersion: exteranto.version
+  })
+
+  localStorage.setItem('@exteranto', JSON.stringify({ ...exteranto, version }))
+}
+
+/**
+ * Gets exteranto internal info from local storage.
+ *
+ * @return {any}
+ */
+const getExterantoInfo: () => any = () : any => {
+  try {
+    return JSON.parse(localStorage.getItem('@exteranto')) || {}
+  } catch (_) {
+    localStorage.setItem('@exteranto', '{}')
+
+    return {}
+  }
 }
