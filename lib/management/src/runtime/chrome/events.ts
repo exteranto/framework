7179a97d569aa4cd04ffbd7ba32c46342da29cc1
Pavel Koch <kouks.koch@gmail.com>
2019-01-14 17:52:54 +0000

    added url filter chrome runtime

--- lib/management/src/runtime/chrome/events.ts
+++ lib/management/src/runtime/chrome/events.ts
@@ -17,11 +17,13 @@ export const register: (dispatcher: Dispatcher) => void = (dispatcher) => {
     dispatcher.mail(`${namespace}.${route}`, event)
   })
 
+  const filter: any = { urls: ['<all_urls>'] }
+
   chrome.webRequest.onBeforeRedirect.addListener((event) => {
     dispatcher.fire(`${namespace}.web-request.before-redirected`, event)
-  })
+  }, filter)
 
   chrome.webRequest.onCompleted.addListener((event) => {
     dispatcher.fire(`${namespace}.web-request.completed`, event)
-  })
+  }, filter)
 }
