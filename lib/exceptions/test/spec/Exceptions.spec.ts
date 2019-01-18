4c9394464043102a9f8ff5ee1c9e26b98a4c14c5
Pavel Koch <kouks.koch@gmail.com>
2019-01-18 18:04:54 +0000

    added tests for exceptions exports

---
+++ lib/exceptions/test/spec/Exceptions.spec.ts
@@ -0,0 +1,17 @@
+import { expect } from 'chai'
+
+import * as e from '../../src'
+
+describe('Exceptions package', () => {
+
+  it('exports global/Exception', () => expect(e.Exception).to.be.ok)
+  it('exports global/EmptyResponseException', () => expect(e.EmptyResponseException).to.be.ok)
+  it('exports global/NotImplementedException', () => expect(e.NotImplementedException).to.be.ok)
+  it('exports global/InvalidUrlFormatException', () => expect(e.InvalidUrlFormatException).to.be.ok)
+  it('exports compatibility/VersionNotMatchedException', () => expect(e.VersionNotMatchedException).to.be.ok)
+  it('exports core/InvalidRouteException', () => expect(e.InvalidRouteException).to.be.ok)
+  it('exports permissions/PermissionNotGrantedException', () => expect(e.PermissionNotGrantedException).to.be.ok)
+  it('exports cookies/InvalidCookieRequestException', () => expect(e.InvalidCookieRequestException).to.be.ok)
+  it('exports tabs/TabIdUnknownException', () => expect(e.TabIdUnknownException).to.be.ok)
+
+})
