3908ac29f25cc9cd1ebf9d79c6f4478858a8d78c
Pavel Koch <kouks.koch@gmail.com>
2019-01-18 11:11:15 +0000

    cleaned up exports for exceptions

--- lib/exceptions/src/index.ts
+++ lib/exceptions/src/index.ts
@@ -1,24 +1,6 @@
-
-export * from './core'
-export * from './tabs'
-export * from './storage'
-export * from './management'
 export * from './compatibility'
-
-export class Exception extends Error {
-  //
-}
-
-export class EmptyResponseException extends Error {
-  //
-}
-
-export class NotImplementedException extends Error {
-  constructor (...args: string[]) {
-    super(args.join(','))
-  }
-}
-
-export class InvalidUrlFormatException extends Error {
-  //
-}
+export * from './core'
+export * from './global'
+export * from './management'
+export * from './storage'
+export * from './tabs'
--- lib/exceptions/src/management/index.ts
+++ lib/exceptions/src/management/index.ts
@@ -1,11 +1,4 @@
 
 export class PermissionNotGrantedException extends Error {
-  /**
-   * Exception name.
-   *
-   * @return {string}
-   */
-  get name () : string {
-    return 'app.exceptions.management.permissionNotGranted'
-  }
+  //
 }
