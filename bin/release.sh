128bec8b51bba44a79fad8bab4d006e5342611ad
Pavel Koch <kouks.koch@gmail.com>
2019-01-21 12:38:52 +0000

    fixed release script

--- bin/release.sh
+++ bin/release.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 ## Login to the NPM registry.
-# echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
 
 
 ### @exteranto/exceptions ###
 cd lib/exceptions
 
-npm version ${TRAVIS_TAG/v/^} --allow-same-version --no-git-tag-version
+npm version ${TRAVIS_TAG/v/} --allow-same-version --no-git-tag-version
 npm publish --access public
 
 sleep 5
 ### @exteranto/core ###
 cd ../core
 
-npm version ${TRAVIS_TAG/v/^} --allow-same-version --no-git-tag-version
-npm i @exteranto/exceptions@${TRAVIS_TAG/v/^}
+npm version ${TRAVIS_TAG/v/} --allow-same-version --no-git-tag-version
+npm i @exteranto/exceptions@${TRAVIS_TAG/v/}
 npm run build
 npm publish --access public
 
@@ -23,8 +23,8 @@ sleep 5
 ### @exteranto/api ###
 cd ../api
 
-npm version ${TRAVIS_TAG/v/^} --allow-same-version --no-git-tag-version
-npm i @exteranto/exceptions@${TRAVIS_TAG/v/^} @exteranto/core@${TRAVIS_TAG/v/^}
+npm version ${TRAVIS_TAG/v/} --allow-same-version --no-git-tag-version
+npm i @exteranto/exceptions@${TRAVIS_TAG/v/} @exteranto/core@${TRAVIS_TAG/v/}
 npm run build
 npm publish --access public
 
@@ -32,7 +32,7 @@ sleep 5
 ### @exteranto/utils ###
 cd ../utils
 
-npm version ${TRAVIS_TAG/v/^} --allow-same-version --no-git-tag-version
-npm i @exteranto/exceptions@${TRAVIS_TAG/v/^} @exteranto/core@${TRAVIS_TAG/v/^} @exteranto/api@${TRAVIS_TAG/v/^}
+npm version ${TRAVIS_TAG/v/} --allow-same-version --no-git-tag-version
+npm i @exteranto/exceptions@${TRAVIS_TAG/v/} @exteranto/core@${TRAVIS_TAG/v/} @exteranto/api@${TRAVIS_TAG/v/}
 npm run build
 npm publish --access public
