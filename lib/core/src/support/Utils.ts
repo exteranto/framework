26635906d32e092bf6ed9b6876593706cd76b78b
Haydn Appleby <haydnba@pm.me>
2020-03-20 17:31:31 +0000

    setup support utils to handle extra browsers: 1. add 'Edge' and 'Opera' items to the Browsers enumeration 2. add conditionals for Edge and Opera browser to 'currentBrowser' util

--- lib/core/src/support/Utils.ts
+++ lib/core/src/support/Utils.ts
@@ -19,7 +19,14 @@ export class Utils {
     const chrome: boolean = !!(window as any).chrome
       && (!!(window as any).chrome.webstore || !!(window as any).chrome.runtime)
 
+    const edge: boolean = chrome && (navigator.userAgent.indexOf("Edg") != -1)
+
+    const opera: boolean = (!!(window as any).opr && !!(window as any).addons)
+      || !!(window as any).opera || navigator.userAgent.indexOf(' OPR/') >= 0
+
     return extensions ? Browser.EXTENSIONS
+      : opera ? Browser.OPERA
+      : edge ? Browser.EDGE
       : chrome ? Browser.CHROME
       : Browser.SAFARI
   }
