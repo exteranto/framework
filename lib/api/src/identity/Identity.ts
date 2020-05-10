21bba5f603f9eb3b3946324047a839621f23611e
Haydn Appleby <haydnba@pm.me>
2020-05-10 22:58:12 +0100

    update comments/docs

--- lib/api/src/identity/Identity.ts
+++ lib/api/src/identity/Identity.ts
@@ -2,18 +2,20 @@
 export abstract class Identity {
 
   /**
-   * Doc...
+   * Generates a url to be used as authentication redirect url.
    *
-   * @param path
-   * @return
+   * @param path A pathname to append to the origin
+   * @return Authentication redirect url
    */
   public abstract getRedirectUrl (path: string) : string
 
   /**
-   * Doc...
+   * Performs first steps of OAuth2 flow, including authenticating user with the
+   * service provider and and handling client authorization.
    *
-   * @param url
-   * @param interactive
+   * @param url The url required by service provider to grant access token
+   * @param interactive Whether flow should complete/fail silently
+   * @returns The redirect url + credentials
    */
   public abstract launchAuthFlow (url: string, interactive?: boolean) : Promise<string>
 
