d0ecd637636e4335d324e5d1707d0c665ea4ec74
Haydn Appleby <haydnba@pm.me>
2020-08-27 12:44:08 +0100

    updates and adds tests to handle the new WindowContentEvent registration/dispatch

--- lib/core/test/spec/app/App.spec.ts
+++ lib/core/test/spec/app/App.spec.ts
@@ -4,7 +4,7 @@ import { mock, instance, verify, deepEqual, when, anything } from 'ts-mockito'
 import { Container } from '@internal/ioc'
 import { Dispatcher, Event } from '@internal/events'
 import { Provider, Script } from '@internal/support'
-import { App, WindowLoadedEvent, AppBootedEvent } from '@internal/app'
+import { App, WindowContentEvent, WindowLoadedEvent, AppBootedEvent } from '@internal/app'
 
 describe('App', () => {
   let container: Container
@@ -79,6 +79,16 @@ describe('App', () => {
     verify(dispatcher.fire(deepEqual(new AppBootedEvent))).once()
   })
 
+  it('registers the window content event', () => {
+    const app: App = new App(Script.BACKGROUND, { providers: [] }, () => {})
+
+    ;(app as any).dispatcher = instance(dispatcher)
+
+    app.bootstrap()
+
+    verify(dispatcher.mail(deepEqual(new WindowContentEvent))).once()
+  })
+
   it('registers the window load event', () => {
     const app: App = new App(Script.BACKGROUND, { providers: [] }, () => {})
 
@@ -104,6 +114,41 @@ describe('App', () => {
 
     verify(dispatcher.mail(deepEqual(new WindowLoadedEvent))).once()
   })
+
+  it('fire the window content event if window is already interactive', () => {
+    const app: App = new App(Script.BACKGROUND, { providers: [] }, () => {})
+
+    ;(app as any).dispatcher = instance(dispatcher)
+
+    const referenceStore = (global as any).window.addEventListener
+    ;(global as any).window.addEventListener = () => {}
+    ;(global as any).document.readyState = 'interactive'
+
+    app.bootstrap()
+
+    ;(global as any).window.addEventListener = referenceStore
+
+    verify(dispatcher.mail(deepEqual(new WindowContentEvent))).once()
+  })
+
+  it('does not fire the window content event if window is already loaded', () => {
+    const app: App = new App(Script.BACKGROUND, { providers: [] }, () => {})
+
+    ;(app as any).dispatcher = instance(dispatcher)
+
+    const referenceStore = (global as any).window.addEventListener
+    ;(global as any).window.addEventListener = () => {}
+    ;(global as any).document.readyState = 'complete'
+
+    app.bootstrap()
+
+    ;(global as any).window.addEventListener = referenceStore
+
+    verify(dispatcher.mail(deepEqual(new WindowLoadedEvent))).once()
+    verify(dispatcher.mail(deepEqual(new WindowContentEvent))).never()
+
+  })
+
 
 })
 
